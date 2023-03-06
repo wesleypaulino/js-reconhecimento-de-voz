66ec5e107ff41a117dedd166476bf44ac4559a76
Wesley Crus <sistemaswesley@gmail.com>
2023-03-06 16:57:21 -0300

    Adicionado funcao reconhecimento-voz

--- js/reconhecimento-voz.js
+++ js/reconhecimento-voz.js
@@ -4,3 +4,9 @@ window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br';
 recognition.start();
+
+recognition.addEventListener( "result", onSpeak)
+
+function onSpeak(e) {
+    return console.log(e.results[0][0].transcript)
+}
